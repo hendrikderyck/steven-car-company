77e56f5750175b9d86da31c088f6f9fae025262a
Drikkie1608 <hendrik.deryck@hotmail.com>
2026-02-05 15:50:01 +0100

    nieuw

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@tailwindcss/vite';
-import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
   integrations: [react()],
   vite: {
     plugins: [tailwind()],
